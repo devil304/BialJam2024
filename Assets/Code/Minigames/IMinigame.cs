b80de0c9684623ea22c8c35d08b4aa065f841800
Szymon Rzosiński <szymonrzosinski@gmail.com>
2024-05-30 20:31:34 +0200

    MiniGame interface

---
+++ Assets/Code/Minigames/IMinigame.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public interface IMinigame
+{
+    public Action MinigameFinished { get; set; }
+    public StatsModel GetStatsFromGame();
+    public void CloseGame();
+    public void ShowGame();
+}
