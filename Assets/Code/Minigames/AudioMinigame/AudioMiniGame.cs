6ad869523a184318f10f0d4f05dc23a4ff92a0fe
Szymon Rzosiński <szymonrzosinski@gmail.com>
2024-05-30 22:17:59 +0200

    Minigame interface changes

--- Assets/Code/Minigames/AudioMinigame/AudioMiniGame.cs
+++ Assets/Code/Minigames/AudioMinigame/AudioMiniGame.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class AudioMiniGame : MonoBehaviour, IMinigame
 {
-    public Action MinigameFinished { get; set; }
-    public StatsModel GetStatsFromGame() {
-			return new StatsModel((0,0,0,5,0));
-		}
-    public void CloseGame() {
-			gameObject.SetActive(false);
-			Debug.Log("Close Game");
-		}
+	public Action MinigameFinished { get; set; }
+	public StatsModel GetStatsFromGame()
+	{
+		return new StatsModel((0, 0, 0, 5, 0));
+	}
+	public void CloseGame()
+	{
+		gameObject.SetActive(false);
+		Debug.Log("Close Game");
+	}
 
-    public void ShowGame() {
-			gameObject.SetActive(true);
-			spawnArrowsManager.StartGame(30f);
-			DOVirtual.DelayedCall(35f, GameOver, false);
-			Debug.Log("Show Game");
-		}
+	public void ShowGame()
+	{
+		gameObject.SetActive(true);
+		spawnArrowsManager.StartGame(30f);
+		DOVirtual.DelayedCall(35f, GameOver, false);
+		Debug.Log("Show Game");
+	}
 
-		public bool IsDisplayed() => gameObject.activeInHierarchy;
+	public bool IsDisplayed => gameObject.activeInHierarchy;
 
-		[SerializeField]
-		SpawnArrowsManager spawnArrowsManager;
+	[SerializeField]
+	SpawnArrowsManager spawnArrowsManager;
 
-		[SerializeField]
-		HitPointsArrowManager hitPointsArrowManager;
+	[SerializeField]
+	HitPointsArrowManager hitPointsArrowManager;
 
-		public void GameOver() {
-			MinigameFinished?.Invoke();
-			CloseGame();
-		}
+	public void GameOver()
+	{
+		MinigameFinished?.Invoke();
+		CloseGame();
+	}
 }
