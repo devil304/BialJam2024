b4b0408a87e5a4c5874a18f48258000b0a340e7f
Tylyps <jakubb.chmielewskii@gmail.com>
2024-05-30 22:54:15 +0200

    Init programing mini game

---
+++ Assets/Code/Minigames/ProgramingMinigame/ProgramingMinigame.cs
@@ -0,0 +1,39 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class ProgramingMinigame : MonoBehaviour, IMinigame
+{
+    public Action MinigameFinished { get; set; }
+    public StatsModel GetStatsFromGame() {
+			return new StatsModel((5,0,0,0,0));
+		}
+    public void CloseGame() {
+			transform.localScale = new Vector3(1, 1, 1);
+			transform.DOScale(0, initializationTime);
+			transform.DOJump(Vector3.zero, 1f, 1, initializationTime);
+			DOVirtual.DelayedCall(initializationTime, () => gameObject.SetActive(false), false);
+			gameOverTween?.Kill();
+			Debug.Log("Close Game");
+		}
+
+    public void ShowGame() {
+			gameObject.SetActive(true);
+			transform.localScale = new Vector3(0, 0, 0);
+			transform.DOScale(1f, initializationTime);
+			transform.DOJump(Vector3.zero, 1f, 1, initializationTime);
+			gameOverTween = DOVirtual.DelayedCall(35f, GameOver, false);
+			Debug.Log("Show Game");
+		}
+
+	public bool IsDisplayed => gameObject.activeInHierarchy;
+
+	Tween gameOverTween;
+	private float initializationTime = 0.6f;
+
+	public void GameOver()
+	{
+		MinigameFinished?.Invoke();
+		CloseGame();
+	}
+}
