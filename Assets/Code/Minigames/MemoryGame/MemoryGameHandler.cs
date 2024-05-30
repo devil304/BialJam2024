97bda9485ea1a8c068970c13607be721abf7e4b9
BlackSmith44 <michal.kow98@interia.pl>
2024-05-30 22:53:25 +0200

    Changes To Memory MiniGame

--- Assets/Code/Minigames/MemoryGame/MemoryGameHandler.cs
+++ Assets/Code/Minigames/MemoryGame/MemoryGameHandler.cs
@@ -5,30 +5,38 @@ using UnityEngine.InputSystem;
 using System;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using TMPro;
 
-public class MemoryGameHandler : MonoBehaviour
+public class MemoryGameHandler : MonoBehaviour, IMinigame
 {
 
     public List<MemoryGameCardScript> flippedCards;
+    public float milliseconds, seconds, minutes, hours;
+    public TMP_Text countdownTimerText;
+    private float currTime;
+    public float countdownTime;
+    StatsModel gameStats;
+
+    [SerializeField]
+    AnimationCurve timer;
+
+    public Action MinigameFinished { get ; set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnEnable()
+    {
+        countdownTime = timer.Evaluate(GameManager.Instance.StatsTeam.Design);
+
+        
+        flippedCards = new();
+
+        InitCards(countdownTime);
+    }
+
     void Start()
     {
 
-        transform.DOMove(Vector3.zero, 0.5f);
-        flippedCards = new();
-        InitCards();
-
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(flippedCards.Count == 2)
-        {
-            CheckCards(flippedCards[0], flippedCards[1]);
-        }
     }
 
     void CheckCards(MemoryGameCardScript a, MemoryGameCardScript b)
@@ -46,8 +54,9 @@ public class MemoryGameHandler : MonoBehaviour
         flippedCards = new();
     }
 
-    void InitCards()
+    void InitCards(float time)
     {
+        currTime = time;
         List<int> idList = new(){ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
 
         foreach (Transform child in transform)
@@ -58,8 +67,58 @@ public class MemoryGameHandler : MonoBehaviour
                 child.GetComponent<MemoryGameCardScript>().cardNum = idList[random];
                 idList.RemoveAt(random);
             }
-
         }
 
     }
+
+    void Update()
+    {
+        if (flippedCards.Count == 2)
+        {
+            CheckCards(flippedCards[0], flippedCards[1]);
+        }
+
+        if(transform.childCount-2 != 0)
+        {
+            currTime -= Time.deltaTime;
+            TimeSpan time = TimeSpan.FromSeconds(currTime);
+            countdownTimerText.text = time.Seconds.ToString() + ":" + time.Milliseconds.ToString();
+
+            if (currTime <= 0)
+            {
+                GameOver(transform.childCount-2);
+                countdownTimerText.text = "00:000";
+            }
+        }
+        else
+        {
+            GameOver(Mathf.CeilToInt(countdownTime - currTime) + 16);
+        }
+
+    }
+
+
+    void  GameOver(float score)
+    {
+        gameStats = new((0, score, 0, 0, 0));
+        MinigameFinished?.Invoke();
+    }
+
+    public StatsModel GetStatsFromGame()
+    {
+        return gameStats;
+    }
+
+    public void CloseGame()
+    {
+        transform.DOMove(new Vector3(0,20,0), 0.5f).OnComplete(() => this.gameObject.SetActive(false));
+    }
+
+    public void ShowGame()
+    {
+        this.gameObject.SetActive(true);
+        transform.DOMove(Vector3.zero, 0.5f);
+    }
+
+    public bool IsDisplayed => gameObject.activeInHierarchy;
 }
