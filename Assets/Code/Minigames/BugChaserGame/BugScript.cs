df95d84ee6f0cb00df601a0536d5ac73ef62d69f
BlackSmith44 <michal.kow98@interia.pl>
2024-06-01 01:15:09 +0200

    bugxD

--- Assets/Code/Minigames/BugChaserGame/BugScript.cs
+++ Assets/Code/Minigames/BugChaserGame/BugScript.cs
@@ -85,7 +85,7 @@ public class BugScript : MonoBehaviour
     IEnumerator WaitAfterDead()
     {
         GetComponent<CapsuleCollider2D>().enabled = false;
-        handler.score += 1;
+        handler.score += 3;
         GetComponent<SpriteRenderer>().sprite = ded;
         source.DOFade(0, 0.5f);
         transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
