6b27e9fd1682528dd2bb348908cb1ab9aeaaff81
Tylyps <jakubb.chmielewskii@gmail.com>
2024-05-31 02:08:33 +0200

    Progress on programing mingame with start, end and score

--- Assets/Code/Minigames/ProgramingMinigame/ProgramingMinigame.cs
+++ Assets/Code/Minigames/ProgramingMinigame/ProgramingMinigame.cs
@@ -22,6 +22,7 @@ public class ProgramingMinigame : MonoBehaviour, IMinigame
 			transform.localScale = new Vector3(0, 0, 0);
 			transform.DOScale(1f, initializationTime);
 			transform.DOJump(Vector3.zero, 1f, 1, initializationTime);
+			DOVirtual.DelayedCall(initializationTime + 0.5f, StartGame, false);
 			gameOverTween = DOVirtual.DelayedCall(35f, GameOver, false);
 			Debug.Log("Show Game");
 		}
@@ -35,5 +36,13 @@ public class ProgramingMinigame : MonoBehaviour, IMinigame
 	{
 		MinigameFinished?.Invoke();
 		CloseGame();
+		wordSpawner.EndGame();
 	}
+
+	public void StartGame() {
+		wordSpawner.StartGame(30f);
+	}
+
+	[SerializeField]
+	WordSpawner wordSpawner;
 }
---
+++ Assets/Code/Minigames/ProgramingMinigame/WordSpawner.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour {
+	[SerializeField]
+	private List<string> keywords = new List<string>();
+
+
+	[SerializeField] TextMeshProUGUI wordToWriteLabel;
+	[SerializeField] TextMeshProUGUI scoreLabel;
+	[SerializeField] TMP_InputField inputField;
+
+	private int score = 0;
+
+	private string wordToWrite;
+	int currentCharIndex;
+
+	bool tryFocus = false;
+
+	private float gameTime = 0;
+	private float maxGameTime = 0;
+
+	private bool shouldSpawn = false;
+
+	private void Start() {
+		inputField.onValueChanged.AddListener(OnInputFieldChanged);
+		inputField.onDeselect.AddListener((string a) => {
+			tryFocus = true;
+		});
+		inputField.onSelect.AddListener((string a) => {
+			tryFocus = false;
+		});
+	}
+
+	private void Update() {
+		if (gameTime >= maxGameTime || !shouldSpawn) return;
+
+		gameTime += Time.deltaTime;
+
+		if (tryFocus && gameObject.activeInHierarchy && shouldSpawn) inputField.Select();
+	}
+
+	public void StartGame(float maxGameTime) {
+		this.maxGameTime = maxGameTime;
+		gameTime = 0;
+		score = 0;
+		wordToWriteLabel.text = "";
+		shouldSpawn = true;
+		inputField.Select();
+		SetNewWord();
+	}
+
+	public void EndGame() {
+		shouldSpawn = false;
+		tryFocus = false;
+	}
+
+	private void SetNewWord() {
+		wordToWrite = GetNewWord();
+		wordToWriteLabel.text = wordToWrite.Replace(" ", "_");
+		currentCharIndex = 0;
+		scoreLabel.text = score.ToString();
+	}
+
+	private void OnInputFieldChanged(string newValue) {
+		if (newValue == "") return;
+
+		char userInput = newValue[0];
+		inputField.text = "";
+
+		HandleUserInput(userInput);
+	}
+
+	private string GetNewWord() {
+		return keywords[Random.Range(0, keywords.Count)];
+	}
+
+	void HandleUserInput(char userInput) {
+		if (CheckChar(wordToWrite[currentCharIndex], userInput)) {
+			currentCharIndex++;
+			wordToWriteLabel.text = GetCorrectLetter(currentCharIndex);
+
+			if (currentCharIndex == wordToWrite.Length) {
+				score += currentCharIndex;
+				SetNewWord();
+			}
+		} else {
+			Debug.Log("INCORRECT");
+		}
+	}
+
+	bool CheckChar(char letterFromWord, char userInput) {
+		if (letterFromWord == ' ') return userInput == ' ' || userInput == '_';
+		return userInput == letterFromWord;
+	}
+
+	string GetCorrectLetter(int currentPosition) {
+		string replacedWord = wordToWrite.Replace(" ", "_");
+		return $"<color=\"green\">{replacedWord.Substring(0, currentPosition)}</color>{replacedWord.Substring(currentPosition, replacedWord.Length - currentPosition)}";
+	}
+}
