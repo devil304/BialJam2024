cfed0ca638e3be2ddbb5c826a9feccbc31ba2605
Szymon Rzosiński <szymonrzosinski@gmail.com>
2024-05-31 22:42:06 +0200

    yes

--- Assets/Code/GameMain/GameplayManager.cs
+++ Assets/Code/GameMain/GameplayManager.cs
@@ -11,6 +11,9 @@ public class GameplayManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI _timeUI;
     [SerializeField] BounceHead[] _teamSprites;
     float _timer;
+    float _mGameTimer;
+    float _anomalyTimer;
+    IMinigame _actMG = null;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,11 +24,13 @@ public class GameplayManager : MonoBehaviour
             var charM = GameManager.I.Team[i];
             _teamSprites[i].UpdateSprites(charM.head, charM.hair, charM.accessory, charM.NickName);
         }
+        _mGameTimer = StrongRandom.RNG.Next(6, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_actMG != null) return;
         if (_timer > 0)
         {
             _timer -= Time.deltaTime;
@@ -38,11 +43,30 @@ public class GameplayManager : MonoBehaviour
             enabled = false;
             return;
         }
+        if (_mGameTimer > 0)
+        {
+            _mGameTimer -= Time.deltaTime;
+            if (_mGameTimer <= 0)
+            {
+                _actMG = GameManager.I._minigames[StrongRandom.RNG.Next(0, 5)];
+                _actMG.ShowGame();
+                _actMG.MinigameFinished += MGFinished;
+            }
+        }
         GameManager.I.ModifyStatsBasedOnTeam(_passivePointsSpeed * Time.deltaTime);
         GameManager.I.StatsAct.Clamp();
         for (int i = 0; i < _statSliders.Length; i++)
         {
             _statSliders[i].value = GameManager.I.StatsAct.GetStat((StatsTypes)i);
         }
+    }
+
+    private void MGFinished()
+    {
+        _actMG.MinigameFinished -= MGFinished;
+        GameManager.I.ModifyStats(_actMG.GetStatsFromGame());
+        _actMG.CloseGame();
+        _actMG = null;
+        _mGameTimer = StrongRandom.RNG.Next(6, 10);
     }
 }
